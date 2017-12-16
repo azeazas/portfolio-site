cf8287445799fada97d05979fe27133135f5ab88
Alexa Zeazas <amzeazas@comcast.net>
2017-12-16 01:02:00 -0800

    ci: update S3 deploys

---
+++ webpack.config.babel.js
@@ -0,0 +1,68 @@
+import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import webpack from 'webpack';
+import childProcess from 'child_process';
+
+const isProduction = (process.env.NODE_ENV === 'production');
+
+const getGitRev = () => process.env.CIRCLE_SHA1
+  || childProcess.execSync('git rev-parse HEAD').toString().trim();
+
+const staticFolder = isProduction
+  ? getGitRev()
+  : 'static';
+
+// locally, we cannot have a leading slash. In production, though,
+// we want one, because otherwise the request will not be relative to the root.
+const staticFolderWithSlash = `${isProduction ? '/' : ''}${staticFolder}`;
+
+export default {
+  entry: {
+    [`${staticFolderWithSlash}/bundle.js`]: [
+      './src/index',
+    ].concat(
+      isProduction
+        ? []
+        : [
+          'webpack/hot/only-dev-server',
+          'webpack-dev-server/client?http://localhost:3000',
+        ]
+      ),
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx'],
+  },
+  output: {
+    filename: '[name]',
+    path: path.resolve('dist'),
+    publicPath: '',
+  },
+  devtool: isProduction ? undefined : '#cheap-module-eval-source-map',
+  module: {
+    loaders: [
+
+      // JS
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loaders: (isProduction ? [] : ['react-hot'])
+          .concat(['babel']),
+      },
+
+      // LESS
+      { test: /\.less$/, loader: 'style!css!less' },
+
+      // images
+      { test: /\.(jpeg|png|jpg)$/, loader: `file?name=${staticFolderWithSlash}/[name].[ext]` },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.tpl.html',
+      filename: path.join(isProduction ? staticFolder : '', 'index.html'),
+    }),
+  ].concat(isProduction
+    ? []
+    : [new webpack.HotModuleReplacementPlugin()]
+  ),
+};
